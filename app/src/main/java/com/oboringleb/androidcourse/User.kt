2768334940a6e9ed8d0a0fde8124cb9debd29bca
Gleb Oborin <Gleb.Oborin@jetbrains.com>
2022-02-21 01:41:50 +0300

    implemented user list representation

---
+++ app/src/main/java/com/oboringleb/androidcourse/User.kt
@@ -0,0 +1,11 @@
+package com.oboringleb.androidcourse
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class User(
+    @Json(name = "avatar") val avatarURL: String,
+    @Json(name = "first_name") val userName: String,
+    @Json(name = "email") val groupName: String,
+)
