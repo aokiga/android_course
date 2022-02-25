ebaa1d9cd81e8b4cdb1b5a55d02fd95a8bee6535
Gleb Oborin <Gleb.Oborin@jetbrains.com>
2022-02-25 05:34:12 +0300

    auth

--- app/src/main/java/com/oboringleb/androidcourse/repository/AuthRepository.kt
+++ app/src/main/java/com/oboringleb/androidcourse/repository/AuthRepository.kt
@@ -7,8 +7,35 @@ object AuthRepository {
     private val _isAuthorizedFlow = MutableStateFlow(false)
     val isAuthorizedFlow = _isAuthorizedFlow.asStateFlow()
 
-    suspend fun signIn() {
+    suspend fun signIn(
+        email: String,
+        password: String
+    ) {
         _isAuthorizedFlow.emit(true)
+        //TODO: Get API response for email availability, change screen to email confirm
+    }
+
+    suspend fun signUp(
+        firstname: String,
+        lastname: String,
+        nickname: String,
+        email: String,
+        password: String
+    ) {
+        //_isAuthorizedFlow.emit(true)
+        //TODO: Get API response for email availability, change screen to email confirm
+    }
+
+    suspend fun verifyCode(
+        firstname: String,
+        lastname: String,
+        nickname: String,
+        email: String,
+        password: String,
+        code: String
+    ) {
+        _isAuthorizedFlow.emit(true)
+        //TODO: Get API response for email availability, change screen to email confirm
     }
 
     suspend fun logout() {
